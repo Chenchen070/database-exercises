22b62451a6a02501687121d28e1d05828f029851
chenchen <chenchenfeng79@gmail.com>
2022-05-19 12:30:07 -0400

    SQL order by exercises 20220519

---
+++ order_by_exercises.sql
@@ -0,0 +1,47 @@
+use employees;
+
+-- 2
+select * from employees
+where first_name in ('Irena', 'Vidya', 'Maya')
+order by first_name;
+-- first row of the results : Irena Reutenauer
+-- last row of the results: Vidya Simmen
+
+-- 3
+select * from employees
+where first_name in ('Irena', 'Vidya', 'Maya')
+order by first_name asc, last_name asc;
+-- first row : Irena Acton
+-- last row : Vidya Zweizig
+
+-- 4
+select * from employees
+where first_name in ('Irena', 'Vidya', 'Maya')
+order by last_name asc, first_name asc;
+-- first row : Irena Acton
+-- last row : Maya Zyda
+
+-- 5
+select * from employees
+where last_name like 'E%' and last_name like '%E'
+order by emp_no;
+-- 899
+-- first : '10021', 'Ramzi', 'Erde'
+-- last : '499648', 'Tadahiro', 'Erde'
+
+-- 6
+select * from employees
+where last_name like 'E%' and last_name like '%E'
+order by hire_date desc;
+-- 899
+-- newest : Teiji Eldridge
+-- oldest : SErgi Erde
+
+-- 7
+select * from employees
+where hire_date like '199%' and birth_date like '%12-25'
+order by birth_date asc, hire_date desc;
+-- 362
+-- oldest employee who was hired last : Khun Bernini
+-- youngest employee who was hired first : Douadi Pettis
+
