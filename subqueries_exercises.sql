f7c9534b0d33baf957a03bf338239a8f58d2e6e2
chenchen <chenchenfeng79@gmail.com>
2022-05-23 14:48:33 -0400

    subqueries exercises bonus added

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -60,5 +60,34 @@ where salary > (select max(salary) - stddev(s.salary)
 				from salaries as s)
 and to_date > now();
 
+-- BONUS
+-- 1. Find all the department names that currently have female managers.
+select d.dept_name from departments as d 
+where dept_no in (
+					select dm.dept_no from dept_manager as dm
+where dm.emp_no in (
+					select e.emp_no from employees as e
+					where gender = 'F' and to_date > now()));
+-- 'Development'
+-- 'Finance'
+-- 'Human Resources'
+-- 'Research'
+
+-- 2. Find the first and last name of the employee with the highest salary.
+select max(salary) from salaries;
+
+select first_name, last_name from employees
+join salaries using (emp_no)
+where salary in (select max(salary) from salaries);
+-- Tokuyasu Pesch
+
+-- 3. Find the department name that the employee with the highest salary works in.
+select dept_name from departments as d
+	join dept_emp as de using (dept_no)
+where emp_no in 
+				(select emp_no from employees
+				join salaries using (emp_no)
+				where salary in (select max(salary) from salaries));
+-- Sales
 
 
