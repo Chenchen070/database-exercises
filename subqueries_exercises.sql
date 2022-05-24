e162b7e71e4a36da4795bd9c59ee58951cc6e555
chenchen <chenchenfeng79@gmail.com>
2022-05-24 10:58:15 -0400

    subqueries_exercises after review

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -8,7 +8,7 @@ where emp_no = 101010)
 and to_date > now();
 
 -- 2. Find all the titles ever held by all current employees with the first name Aamod.
-select * from titles
+select distinct title from titles
 where emp_no in (select emp_no from employees
 where first_name = 'Aamod')
 and to_date > now();
@@ -18,9 +18,9 @@ select emp_no from dept_emp
 where to_date < now();
 
 select count(*) from employees
-where emp_no in (select emp_no from dept_emp
-where to_date < now());
--- 85108
+where emp_no not in (select emp_no from dept_emp
+where to_date > now());
+-- 59900
 
 -- 4 Find all the current department managers that are female. List their names in a comment in your code.
 select emp_no from dept_manager
@@ -60,6 +60,42 @@ where salary > (select max(salary) - stddev(s.salary)
 				from salaries as s)
 and to_date > now();
 
+-- review from instructor
+ # what is the standard devition?
+ select stddev(salary) from salaries where to_date > now();
+ 
+ # what id the max salary?
+ select max(salary) from salaries where to_date > now();
+
+# ount of current salaries > (Max-1 std). (count = 83)
+select count(*)
+from salaries
+where to_date > now()
+and salary > (
+(select max(salary) from salaries where to_date > now()) - 
+(select std(salary) from salaries where to_date > now())
+);
+
+-- Denominator, count of all current salaries (240,124)
+select count(*)
+from salaries 
+where to_date > now();
+
+# what percent of overall current salaries?
+-- percent = salaries above the threshold/total current salaties
+-- SELECT (Numerator)/ (Denominator)
+select((select count(*)
+from salaries
+where to_date > now()
+and salary > (
+(select max(salary) from salaries where to_date > now()) - 
+(select std(salary) from salaries where to_date > now())
+))/(select count(*)
+from salaries 
+where to_date > now())) * 100 as "percentage of salaries within 1 Stdev of Max";
+
+
+
 -- BONUS
 -- 1. Find all the department names that currently have female managers.
 select d.dept_name from departments as d 
@@ -89,5 +125,3 @@ where emp_no in
 				join salaries using (emp_no)
 				where salary in (select max(salary) from salaries));
 -- Sales
-
-
